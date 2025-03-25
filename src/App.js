4361a5f4d789bab899123acbca3676363a62678b
gustavorsc <gugwqh@gmail.com>
2025-03-25 20:44:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import './style.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [input, setInput] = useState('');
+
+  function loadAPI() {
+    let url = `https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Pokemon not found');
+        }
+        return response.json();
+      })
+      .then(json => {
+        setPokemon(json);
+      })
+      .catch(err => {
+        console.log(err);
+        setPokemon(null);
+      });
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      loadAPI();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <strong>Pokemon API</strong>
       </header>
+      <div>
+        <input
+          type='text'
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder='Digite o nome do Pokémon'
+        />
+        <button onClick={loadAPI}>Buscar</button>
+      </div>
+      {pokemon ? (
+        <div className='pokemon'>
+          <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
+          <div>Nome: {pokemon.name}</div>
+          <div>Nº: {pokemon.id}</div>
+          <div>Peso: {pokemon.weight / 10} kg</div>
+          <div>Altura: {pokemon.height / 10} m</div>
+        </div>
+      ) : (
+        <p>Pokemon não encontrado</p>
+      )}
     </div>
   );
 }
